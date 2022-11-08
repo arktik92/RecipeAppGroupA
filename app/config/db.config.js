fb6adf2875d954a60bfbbae86636657c1f316c9b
Esteban SEMELLIER <esteban.semellier@gmail.com>
2022-11-08 18:36:23 +0100

    reparation ok ?

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,24 +1,25 @@
 
 
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: "postgres",
-  operatorsAliases: "0",
+// const Sequelize = require('sequelize');
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//   host: process.env.DB_HOST,
+//   dialect: "postgres",
+//   operatorsAliases: "0",
 
-});
+// });
  
 
-// const Sequelize = require('sequelize');
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false
-//       }
-//     }
-//   }
-// );
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
 
 
 sequelize
