92035820c136e5f9fd7643b8052b021601d6405a
Esteban SEMELLIER <esteban.semellier@gmail.com>
2022-11-08 18:41:07 +0100

    reparation ok ?

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ require('./app/routes/favorite.routes.js')(app)
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
